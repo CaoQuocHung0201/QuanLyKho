323e10c592c4bd1f006cb8ff627c46dd5a9b92da
Bonze_Do_71 <duymibo@gmail.com>
2023-04-06 10:34:30 +0700

    QR || Barcode

---
+++ app/src/main/java/com/mibo/quanlykho/Controllers/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.mibo.quanlykho.Controllers;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/mibo/quanlykho/Views/QuetMa.java
+++ app/src/main/java/com/mibo/quanlykho/Views/QuetMa.java
@@ -1,9 +1,14 @@
 package com.mibo.quanlykho.Views;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+import com.mibo.quanlykho.Controllers.CaptureAct;
 import com.mibo.quanlykho.R;
 
 public class QuetMa extends AppCompatActivity {
@@ -12,5 +17,21 @@ public class QuetMa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quet_ma);
+
+        ScanOptions options=new ScanOptions();
+        options.setPrompt("MIBO");
+
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaucher.launch(options);
     }
+    ActivityResultLauncher<ScanOptions> barLaucher=registerForActivityResult(new ScanContract(), result -> {
+        if (result.getContents()!=null){
+//            Intent intent=new Intent(MainActivity.this, ImportActivity.class);
+//            intent.putExtra("id",result.getContents());
+//            startActivity(intent);
+//            finish();
+        }
+    });
 }
